ded31731df5d9c1c902f80737e9ed786f43c7aac
Duy-Nhat <duynhat8987@gmail.com>
2022-07-30 09:28:29 +0700

    fix

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,32 +1,51 @@
+import { useEffect, useState } from 'react';
 import classNames from 'classnames/bind';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMagnifyingGlass, faSpinner, faXmark } from '@fortawesome/free-solid-svg-icons';
 import styles from './Header.module.scss';
 import images from '../../../../assets/images/logo.svg';
+import Tippy from '@tippyjs/react/headless';
 
 const cx = classNames.bind(styles);
 
 function Header() {
+    const [searchResult, setSearchResult] = useState([]);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setSearchResult([1, 2, 3]);
+        }, 3000);
+    }, []);
     return (
         <header className={cx('wrapper')}>
-            <div className={cx('inner')}>
-                <div className={cx('logo')}>
+            <div className={cx('logo')}>
+                <div className={cx('inner')}>
                     <img src={images} alt="Tiktok" />
-                </div>
-                <div className={cx('search')}>
-                    <input placeholder="search accounts and videos" spellCheck={false} />
-                    {/* <div className={cx('component-close')}>
+                    <Tippy
+                        interactive
+                        visible={searchResult.length > 0}
+                        render={(attrs) => (
+                            <div className={cx('search-result')} tabIndex="-1" {...attrs}>
+                                Ket qua
+                            </div>
+                        )}
+                    >
+                        <div className={cx('search')}>
+                            <input placeholder="Hãy bấm vào em " spellCheck={false} />
+                            {/* <div className={cx('component-close')}>
                         <button className={cx('clear')}>
                             <FontAwesomeIcon icon={faXmark} />
                         </button>
                         <FontAwesomeIcon className={cx('loading ')} icon={faSpinner} />
                     </div> */}
 
-                    <button className={cx('search-btn')}>
-                        <FontAwesomeIcon icon={faMagnifyingGlass} />
-                    </button>
+                            <button className={cx('search-btn')}>
+                                <FontAwesomeIcon icon={faMagnifyingGlass} />
+                            </button>
+                        </div>
+                    </Tippy>
+                    <div className={cx('actions')}></div>
                 </div>
-                <div className={cx('actions')}></div>
             </div>
         </header>
     );
